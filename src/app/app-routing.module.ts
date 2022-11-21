da0c8baabeb262bacecc9f7d3ac2c0b93a0ce9b6
Koralek1992 <dkokralek2@gmail.com>
2022-11-21 14:05:16 +0100

    task 3 done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,22 @@ import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {ProductFormComponent} from './components/product-form/product-form.component';
 import {EmployeeFormComponent} from './components/employee-form/employee-form.component';
+import {LoginFormComponent} from './components/login-form/login-form.component';
 import {ProductFormComponentModule} from './components/product-form/product-form.component-module';
 import {ProductServiceModule} from './services/product.service-module';
 import {EmployeeFormComponentModule} from './components/employee-form/employee-form.component-module';
 import {EmployeeFormServiceModule} from './services/employee-form.service-module';
+import {LoginFormComponentModule} from './components/login-form/login-form.component-module';
+import {LoginFormServiceModule} from './services/login-form.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([{path: 'create-product', component: ProductFormComponent}, {
     path: 'create-employee',
     component: EmployeeFormComponent
-  }]), ProductFormComponentModule, ProductServiceModule, EmployeeFormComponentModule, EmployeeFormServiceModule],
+  }, {
+    path: 'login',
+    component: LoginFormComponent
+  }]), ProductFormComponentModule, ProductServiceModule, EmployeeFormComponentModule, EmployeeFormServiceModule, LoginFormComponentModule, LoginFormServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
